6b19c236c60b5fa32069bcfd7af4831f03ff64cf
fazizi123 <fazizi23@gmail.com>
2024-11-11 10:36:12 -0500

    third commit

--- src/main/java/org/example/LebronIsTheBest.java
+++ src/main/java/org/example/LebronIsTheBest.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class LebronIsTheBest {
     public static void main(String[] args) {
-        System.out.println("Alot of people think Lebron isn't the GOAT. I think he's better than Jordan");
+        System.out.println("Lebron continues to be great!");
     }
 }
