4a25bc710879ce779c188532ea343358b5a998ac
fazizi123 <fazizi23@gmail.com>
2024-11-11 10:19:53 -0500

    first commit

---
+++ src/main/java/org/example/LebronIsTheBest.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class LebronIsTheBest {
+    public static void main(String[] args) {
+        System.out.println("Alot of people think Lebron isn't the GOAT.");
+    }
+}
