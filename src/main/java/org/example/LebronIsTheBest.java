46a1213e7305041ae47a3b7da012e93414de6153
fazizi123 <fazizi23@gmail.com>
2024-11-11 10:51:01 -0500

    The last dance

--- src/main/java/org/example/LebronIsTheBest.java
+++ src/main/java/org/example/LebronIsTheBest.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class LebronIsTheBest {
     public static void main(String[] args) {
-        System.out.println("Lebron continues to be great!");
+        System.out.println("Lebron is better than Kobe!!");
     }
 }
